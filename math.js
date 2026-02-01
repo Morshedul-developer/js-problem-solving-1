19dfb5fe77a25087fa8342ae92698ba53252c133
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-01 19:49:37 +0600

    explored js math properties

---
+++ math.js
@@ -0,0 +1,20 @@
+//* min
+// console.log(Math.min(1,2,3,4,5,6,-88));
+//* max
+// console.log(Math.max(1,2,3,4,5,6,-88));
+//* abs
+// console.log(Math.abs(5-10)); // output: 5
+//* round
+// console.log(Math.round(2.25)); // output: 2
+// console.log(Math.round(2.49999)); // output: 2
+// console.log(Math.round(2.5)); // output: 3
+// console.log(Math.round(2.75)); // output: 3
+//* floor
+// console.log(Math.floor(2.75)); // output: 2
+//* ceil
+// console.log(Math.ceil(2.75)); // output: 3
+//* random
+// console.log(Math.random());
+// console.log(Math.random()*10);
+const randomNum = Math.round(Math.random()*10)
+console.log(randomNum);
