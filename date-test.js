8e77bd15df7ac671522edf9bbe6f837eb2c3974f
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-02-04 15:00:03 +0600

    explored more about date

---
+++ date-test.js
@@ -0,0 +1,9 @@
+const now = new Date();
+console.log(now.toLocaleTimeString());
+console.log(now.toDateString());
+console.log(now.toTimeString());
+
+// const now = new Date();
+// console.log(now.toLocaleString());
+// console.log(now.toLocaleDateString());
+// console.log(now.toLocaleTimeString());
